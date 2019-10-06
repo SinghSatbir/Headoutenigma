e7ee2af449655071eff5f259f33ae8f23789b019
kaushil <kaushil99@gmail.com>
2019-10-06 10:12:50 +0530

    updated files

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var app = require('express')();
 var request = require('request');
 var bodyParser  = require("body-parser");
-var btoa = require('btoa');
-var fs = require('fs');
 var calc = require("./detecting_obj");
 
 
@@ -13,17 +11,12 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.raw({ type: 'application/octet-stream', limit: '50mb' }));
 
 app.post('/image', (req, res, next) => {
-   // console.log(req);
-    console.log("accept");
     let bbb = req.body;
-    //console.log(req.body);
     if(IsEm(bbb)){
         console.log("error");
         res.status(500).send("please try again");
     }else{
-      //console.log(bbb.tp);  
     let img = Buffer.from(bbb.img, "base64");
-    // let img = bbb;
 
         var options = {
             url: "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/describe?language=en",
@@ -34,15 +27,11 @@ app.post('/image', (req, res, next) => {
             },
             body: img
         };
-        //console.log(options);
         res.status(200);
         request(options ,(err, res1, body) => {
             if(err) {
-               // console.log(err);
             }else {
-                //console.log(body);
                 let bb = JSON.parse(body);
-                console.log(bb);
                 let captions;
                 if(bb.description.captions.length>0){
                     captions = bb.description.captions[0].text + ". ";
@@ -52,7 +41,6 @@ app.post('/image', (req, res, next) => {
                 
                 let height = bb.metadata.height;
                 let width = bb.metadata.width;
-                console.log(captions);
                 request({
                     url: "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/detect",
                     method: "POST",
@@ -62,47 +50,17 @@ app.post('/image', (req, res, next) => {
                     },
                     body: img
                 }, (err1, res2, body1)=> {
-                    //console.log(body1);
                     let bd = JSON.parse(body1);
-                    console.log(bd);
                     let objs = bd.objects;
                     let tt="";
                     objs.forEach(el => {
-                        // if(captions.includes(el.object)){
                             tt=tt+calc(el,width,height) + ". ";
-                        // }
                     });
                     console.log(tt);
                     let reee = captions + ". " + tt;
                     res.status(200).send(reee);
                 })
-                // console.log(typeof(captions));
-                // console.log("sdf  "+captions);
-                // let username = "apikey";
-                // let password = "1yHvWupnIVMBlefOnUgs2Vg8HSEor7yiBd6fHBr3Z4JE";
-                // let basic = btoa(username + ":" + password);
-                // var options1 = {
-                //     method: "POST",
-                //     url: "https://gateway-lon.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=en-US_AllisonVoice",
-                //     headers: {
-                //         "Authorization": `Basic ${basic}`,
-                //         "Content-Type": "application/json",
-                //         "Accept": "audio/wav"
-                //     },
-                //     body: {
-                //         "text": captions
-                //     }, 
-                //     json: true
-                // }
-                // console.log(options1);
-                // request(options1, (errr, ress, body1) => {
-                //     // console.log(body1);
-                //     fs.writeFile("./output.wav", body1, (err)=> {
-                //         if(err) console.log(err);
-                //         console.log("saved");
-                //     })
-                //     res.send("yes");
-                // })
+                
             }    
     })
 }
